ad1fda4c2dcac574c04cb621b91827787e739d9b
SanathVenkatachalam <venkatachalam0362@mydusd.org>
2025-02-04 13:04:45 -0800

    added data

--- Welcome01.java
+++ Welcome01.java
@@ -2,13 +2,11 @@ import core.data.*;
 
 public class Welcome01 {
    public static void main(String[] args) {
-      String id = "KEMT";
-      DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/" + id + ".xml"); 
+
+      DataSource ds = DataSource.connect("data.xml"); 
       ds.setCacheTimeout(15 * 60);  
       ds.load();
-      //ds.printUsageString();
-      float temp = ds.fetchFloat("temp_f");
-      String loc = ds.fetchString("location");
-      System.out.println("The temperature at " + loc + " is " + temp + "F");
+      ds.printUsageString();
+      
    }
 }
--- Welcome03_List.java
+++ Welcome03_List.java
@@ -9,32 +9,14 @@ import java.util.Scanner;
 public class Welcome03_List {
    public static void main(String[] args) {
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
-      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", 
-             "station/station_id", "station/state",
-             "station/latitude", "station/longitude");
-      System.out.println("Total stations: " + allstns.size());
       
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter a state abbreviation: ");
       String state = sc.next();
       System.out.println("Stations in " + state);
-      for (WeatherStation ws : allstns) {
-         if (ws.isLocatedInState(state)) {
-            System.out.println("  " + ws.getId() + ": " + ws.getName());
-         }
-      }
 
       double previousLat = 0.0;
       String stationName ="";
-
-      for(WeatherStation ws : allstns){
-         previousLat = ws.getLatitude();
-         stationName = ws.getName();
-         if(ws.getLatitude() < previousLat){
-            previousLat = ws.getLatitude();
-            stationName = ws.getName();
-         }
-      }
       System.out.println("Southern Most station: "+ stationName + " with latitude of " + previousLat);
    }
 }
